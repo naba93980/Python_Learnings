facdc34212927c184139bcbf3d13bfceb0ba5a65
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-12-26 22:32:21 +0530

    added string functions

--- CodeWithHarry/stringSlicing.py
+++ CodeWithHarry/stringSlicing.py
@@ -8,3 +8,19 @@ print(c[0::2]);
 print(c[7:0:-1])  # roM_doo
 print(c[:2:-1])  # yrraHgninroM_d
 
+story = "once upon a time there was a youtuber named Naba";
+
+# String Functions
+
+# print(len(story));
+
+# print(story.endswith("notes"));
+
+# print(story.count("c"));
+
+# print(story.capitalize());
+
+# print(story.find("upon"));
+
+print(story.replace("Harry", "CodeWithHarry"));
+
