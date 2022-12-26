2b0b3b95f169d0b4b460d3cf3473d3abde4efdab
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-12-26 21:05:36 +0530

    modules import basic

--- CodeWithHarry/firstProg.py
+++ CodeWithHarry/firstProg.py
@@ -1,6 +1,16 @@
-import os;
-import flask;
-print(type(os.path.dirname));
-print(flask);
+import statistics
+import tkinter.ttk
+import turtle
+import os
+import flask
+print(type(os))
+print(flask)
 # print("Hello world");
 # print(type ("hello world"));
+print(turtle)
+print(tkinter.ttk)
+os.DirEntry.path
+import random;
+print(os.__all__); 
+print("---------------------")
+print(dir(os.__name__))
