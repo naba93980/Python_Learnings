9f10e82598007a89675f653efbaf1e399ddf0e91
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-12-26 21:18:13 +0530

    string slicing

---
+++ CodeWithHarry/stringSlicing.py
@@ -0,0 +1,10 @@
+greeting = "Good_Morning";
+name = "Harry";
+# print(type(name));
+c = greeting + name
+print(c);
+print(c[0:(len((c))-1):2]);
+print(c[0::2]);
+print(c[7:0:-1])  # roM_doo
+print(c[:2:-1])  # yrraHgninroM_d
+
